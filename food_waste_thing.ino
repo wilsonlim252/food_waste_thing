cbc9ab164fef4a34491eed96b2d1326c1ca40cbb
wilsonlim252 <85865306+wilsonlim252@users.noreply.github.com>
2026-07-24 13:31:28 +0800

    Add files via upload

---
+++ food_waste_thing.ino
@@ -0,0 +1,107 @@
+#include <TM1637Display.h>
+
+void nullall(void);
+
+const int CLK = 10;
+const int DIO = 11;
+
+TM1637Display display(CLK, DIO);
+
+const int potpin = 0;
+
+const int buzzer = 3;
+const int redled = 4;
+const int yellowled = 7;
+const int greenled = 5;
+
+int thresholdv[] = {600, 800, 900, 1000, 1100, 1200};
+/*
+green < 600
+blink > 600 <= 800
+yellow > 800 <= 900
+blink > 900 <= 1000
+red  > 1000 <= 1100
+blink > 1100 <= 1200
+red > 1200 */
+
+//set blinking interval)
+const int wait = 100;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(potpin, INPUT);
+
+  pinMode(buzzer, OUTPUT);
+  pinMode(redled, OUTPUT);
+  pinMode(yellowled, OUTPUT);
+  pinMode(greenled, OUTPUT);
+
+  display.setBrightness(7);
+}
+
+void loop() {
+  int potv = analogRead(potpin)*2;
+
+  //print my values dawgg
+  Serial.println(potv);
+  display.showNumberDec(potv);
+
+  //Green
+  if (potv <= thresholdv[0]) {
+    nullall();
+    digitalWrite(greenled, HIGH);
+  }
+  else if (potv > thresholdv[0] && potv <= thresholdv[1]) {
+    nullall();
+    digitalWrite(greenled, HIGH);
+    delay(wait);
+    digitalWrite(greenled, LOW);
+    delay(wait);
+  }
+
+  //Yellow
+  else if (potv > thresholdv[1] && potv <= thresholdv[2]) {
+    nullall();
+    digitalWrite(yellowled, HIGH);
+  }
+  else if (potv > thresholdv[2] && potv <= thresholdv[3]) {
+    nullall();
+    digitalWrite(yellowled, HIGH);
+    delay(wait);
+    digitalWrite(yellowled, LOW);
+    delay(wait);
+  }
+
+  //Red
+  else if (potv > thresholdv[3] && potv <= thresholdv[4]) {
+    nullall();
+    digitalWrite(redled, HIGH);
+  }
+  else if (potv > thresholdv[4] && potv < thresholdv[5]) {
+    nullall();
+    digitalWrite(redled, HIGH);
+    delay(wait);
+    digitalWrite(redled, LOW);
+    delay(wait);
+  }
+
+  //Buzzer
+  else if (potv > thresholdv[5]) {
+    nullall();
+    tone(buzzer, 1000);
+    delay(wait);
+    tone(buzzer, 500);
+    delay(wait);
+  }
+  else {
+    nullall();
+  }
+}
+
+void nullall() {
+  digitalWrite(redled, LOW);
+  digitalWrite(yellowled, LOW);
+  digitalWrite(greenled, LOW);
+  noTone(buzzer);
+}
